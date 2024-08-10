7a858471aa04ecdae5e094ef684f56c4972cb341
Evgeny Ukhanov <evgeny@aurora.dev>
2024-08-10 11:19:26 +0200

    Added decoder and decoder, and refactoring

--- Sources/Registers.swift
+++ Sources/Registers.swift
@@ -4,7 +4,7 @@ import Foundation
 /// By default initialised with zero
 struct Registers {
     /// Basic registers count
-    let REGISTERS_COUNT = 32
+    let REGISTERS_COUNT: Register = 32
     /// Registers array of  register data with length `REGISTERS_COUNT`
     private var regs: [MachineValue] = []
     /// Registers bit architecture
@@ -19,30 +19,30 @@ struct Registers {
         case .x64: .x64(0)
         }
         // Init all registers
-        self.regs = Array(repeating: regZeroValue, count: REGISTERS_COUNT)
+        self.regs = Array(repeating: regZeroValue, count: Int(REGISTERS_COUNT))
         // Set register architecture
         self.arch = arch
     }
 
     /// Set register at index with register `Value`
-    mutating func setRegister(at index: Int, with value: MachineValue) {
+    mutating func setRegister(at reg: Register, with value: MachineValue) {
         // Check is index in register file range
-        assert(index < REGISTERS_COUNT, "Index out of bounds")
+        assert(reg < REGISTERS_COUNT, "Index out of bounds")
 
         // Ensure that Register architecture the same as register Value
         switch (arch, value) {
-        case (.x32, .x32): regs[index] = value
-        case (.x64, .x64): regs[index] = value
+        case (.x32, .x32): regs[Int(reg)] = value
+        case (.x64, .x64): regs[Int(reg)] = value
         default: assertionFailure("setRegister Value type does not match register architecture")
         }
     }
 
     /// Get register at index
-    public func getRegister(at index: Int) -> MachineValue {
+    public func getRegister(at reg: Register) -> MachineValue {
         // Check is index in register file range
-        assert(index < REGISTERS_COUNT, "Index out of bounds")
+        assert(reg < REGISTERS_COUNT, "Index out of bounds")
 
-        return regs[index]
+        return regs[Int(reg)]
     }
 }
 
